e5055d1301c928a2d90b73983f3939b5ff1b6c0a
heyanik <anikdaspartha58@gmail.com>
2023-11-04 20:08:15 +0600

    second commit

--- src/Data.js
+++ src/Data.js
@@ -1,16 +1,16 @@
 const imageData = [
-  "./src/assets/image-1.webp",
-  "./src/assets/image-2.webp",
-  "./src/assets/image-3.webp",
-  "./src/assets/image-4.webp",
-  "./src/assets/image-5.webp",
-  "./src/assets/image-6.webp",
-  "./src/assets/image-7.webp",
-  "./src/assets/image-8.webp",
-  "./src/assets/image-9.webp",
-  "./src/assets/image-10.jpeg",
-  "./src/assets/image-11.jpeg",
-  "./src/assets/image-12.png",
+  "assets/image-1.webp",
+  "assets/image-2.webp",
+  "assets/image-3.webp",
+  "assets/image-4.webp",
+  "assets/image-5.webp",
+  "assets/image-6.webp",
+  "assets/image-7.webp",
+  "assets/image-8.webp",
+  "assets/image-9.webp",
+  "assets/image-10.jpeg",
+  "assets/image-11.jpeg",
+  "assets/Image-12.png",
   // Add more image objects as needed
 ];
 
